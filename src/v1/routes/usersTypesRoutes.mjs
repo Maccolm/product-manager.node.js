0c38d8af76e51dc38ac3462dca9e9149b4ff6c86
Vitalii Shapovalov <martyit@ukr.net>
2025-01-08 12:13:57 +0100

    Users types: created: - page - controller - usersTypesApiManager

---
+++ src/v1/routes/usersTypesRoutes.mjs
@@ -0,0 +1,19 @@
+import express from 'express'
+import usersTypesController from '../controllers/usersTypesController.mjs'
+import { getPermissionsChecker } from '../../../middleware/auth.mjs'
+
+const router = express.Router()
+const permissionsChecker = getPermissionsChecker('usersTypes')
+
+router.get('/:id', permissionsChecker('read'), usersTypesController.getById)
+router.get('/', permissionsChecker('read'), usersTypesController.getList)
+
+router.post(
+  '/register/:id?',
+  permissionsChecker('update'),
+  usersTypesController.register
+)
+
+router.delete('/', permissionsChecker('delete'), usersTypesController.delete)
+
+export default router
