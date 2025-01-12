ad7f68392a6f35db840380424eb75172a3240804
Vitalii Shapovalov <martyit@ukr.net>
2025-01-12 11:54:57 +0100

    filtered backend data to filter products retrieval and clean up unused code

--- src/v1/controllers/filtersController.mjs
+++ src/v1/controllers/filtersController.mjs
@@ -1,22 +1,15 @@
-import TypesDBService from '../models/type/TypesDBService.mjs'
-import UsersDBService from '../models/user/UsersDBService.mjs'
+
 import ProvidersDBService from '../models/provider/providersDBService.mjs'
 
 class FilterService {
   static async getFiltersData(req, res) {
     try {
       // Виконання запитів паралельно з використанням Promise.all
-      const [typesList, usersList, providers] = await Promise.all([
-        TypesDBService.getList(),
-        UsersDBService.getListWithoutAdmin(),
-		  ProvidersDBService.getList()
-      ])
+      const providers = await ProvidersDBService.getList()
 
       // Повернення об'єднаних даних
       res.status(200).json({
         data: {
-          types: typesList,
-          users: usersList,
 			 providers
         },
         success: true,
